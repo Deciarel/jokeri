cd90d559ff2db35fa98b136bed76c27e7418ae2b
Deciarel <holtta.kalle@gmail.com>
2024-02-13 12:47:53 +0200

    valmis

--- functions/functions.js
+++ functions/functions.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     luoNappi.addEventListener('click', generateJokerRow);
 
     function generateJokerRow() {
-        const newRow = document.createElement('tr');
+        let newRow = document.createElement('tr');
         for (let i = 0; i < 7; i++) {
-            const randomNumber = Math.floor(Math.random() * 10);
-            const newCell = document.createElement('td');
+            let randomNumber = Math.floor(Math.random() * 10);
+            letgit newCell = document.createElement('td');
             newCell.textContent = randomNumber;
             newRow.appendChild(newCell);
         }
